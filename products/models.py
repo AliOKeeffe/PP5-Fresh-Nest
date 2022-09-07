3a794576dd0d58c92d126abb36675b94746b11bf
Alison O'Keeffe <okeeffe.ali@gmail.com>
2022-09-07 12:49:20 +0000

    Added estimated dispatch to product model

--- products/models.py
+++ products/models.py
@@ -30,6 +30,8 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     excerpt = models.TextField(blank=True)
     description = models.TextField()
+    estimated_dispatch = models.CharField(
+        max_length=254, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(null=True, blank=True)
     liked = models.ManyToManyField(
