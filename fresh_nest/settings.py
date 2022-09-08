9435a97b135f21a66924fa1c5095ab7f54add99f
Alison O'Keeffe <okeeffe.ali@gmail.com>
2022-09-08 16:13:01 +0000

    update image url field in bag and in toast to render if no image

--- fresh_nest/settings.py
+++ fresh_nest/settings.py
@@ -70,10 +70,11 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request', # required by allauth
+                'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
+                'django.template.context_processors.media',
             ],
         },
     },
