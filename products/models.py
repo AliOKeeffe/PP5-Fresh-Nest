c476edefea4097b94d57406a427c4171b34f21df
Alison O'Keeffe <okeeffe.ali@gmail.com>
2022-09-08 15:50:53 +0000

    Add noimage.png

--- products/models.py
+++ products/models.py
@@ -33,6 +33,7 @@ class Product(models.Model):
     estimated_dispatch = models.CharField(
         max_length=254, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     liked = models.ManyToManyField(
         User, related_name='liked', default=None, blank=True)
