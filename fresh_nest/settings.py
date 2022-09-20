7f36f96ab8cdf72edd313f4a2421c11a5f59086d
Alison O'Keeffe <okeeffe.ali@gmail.com>
2022-09-20 11:54:46 +0000

    Removed secret key and set debug

--- fresh_nest/settings.py
+++ fresh_nest/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lw(a-3(6vm^iana&k*rc===a$6hvu@t0n0#9$lj7fp=6curzpu'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['fresh-nest.herokuapp.com', 'localhost']
 
