8d10ca99b70ba6f224f4a0569a6ac33c2913eab7
Alison O'Keeffe <okeeffe.ali@gmail.com>
2022-09-20 11:05:51 +0000

    Deployment part 1

--- fresh_nest/settings.py
+++ fresh_nest/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 from os import path
 if path.exists("env.py"):
@@ -122,11 +123,15 @@ WSGI_APPLICATION = 'fresh_nest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
