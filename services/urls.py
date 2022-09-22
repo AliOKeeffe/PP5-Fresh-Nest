620279f68ed665156b4b4c2a119e0124b41977e3
Alison O'Keeffe <okeeffe.ali@gmail.com>
2022-09-22 15:12:02 +0000

    add edit / delete views for services, at testimonial view

--- services/urls.py
+++ services/urls.py
@@ -4,4 +4,12 @@ from . import views
 urlpatterns = [
     path('', views.Services.as_view(), name='services'),
     path('add/', views.AddService.as_view(), name='add_service'),
+    path(
+        'edit/<int:pk>/', views.EditService.as_view(),
+        name='edit_service'
+        ),
+    path(
+        'delete/<int:pk>/', views.DeleteService.as_view(),
+        name='delete_service'
+        ),
 ]
--- services/views.py
+++ services/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Service, Testimonial, PreviousProject
+from django.urls import reverse_lazy
 from .forms import ServiceForm
 
 
@@ -45,3 +46,73 @@ class AddService(
         """
         if self.request.user.is_superuser:
             return True
+
+
+class EditService(
+        LoginRequiredMixin, UserPassesTestMixin,
+        SuccessMessageMixin, generic.UpdateView
+        ):
+    """
+    This view is used to allow the superuser to edit Service details
+    """
+    model = Service
+    form_class = ServiceForm
+    template_name = 'services/edit_service.html'
+    success_message = "%(calculated_field)s was edited successfully"
+
+    def form_valid(self, form):
+        """
+        This method is called when valid form data has been posted.
+        """
+        return super().form_valid(form)
+
+    def test_func(self):
+        """
+        Ensure only superuser can edit service details
+        """
+        if self.request.user.is_superuser:
+            return True
+
+    def get_success_message(self, cleaned_data):
+        """
+        Override the get_success_message() method to add the service type
+        into the success message.
+        """
+        return self.success_message % dict(
+            cleaned_data,
+            calculated_field=self.object.type,
+        )
+
+
+class DeleteService(
+        LoginRequiredMixin, UserPassesTestMixin, generic.DeleteView):
+    """
+    This view is used to allow logged in users to delete their own recipes
+    """
+    model = Service
+    template_name = 'services/delete_service.html'
+    success_message = "Service deleted successfully"
+    success_url = reverse_lazy('services')
+
+    def test_func(self):
+        """
+       Ensure only superuser can edit service details
+        """
+        if self.request.user.is_superuser:
+            return True
+
+    def delete(self, request, *args, **kwargs):
+        """
+        This function is used to display sucess message given
+        SucessMessageMixin cannot be used in generic.DeleteView.
+        Credit: https://stackoverflow.com/questions/24822509/
+        success-message-in-deleteview-not-shown
+        """
+        messages.success(self.request, self.success_message)
+        return super(DeleteService, self).delete(request, *args, **kwargs)
+
+
+class Testimonials(generic.ListView):
+    """ This view is used to display all testimonials """
+    model = Testimonial
+    template_name = 'services/testimonial.html'
