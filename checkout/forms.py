20971b5e501fb5b1a9cb3bf4a0f29df7402e48a0
Alison O'Keeffe <okeeffe.ali@gmail.com>
2022-10-13 21:09:34 +0000

    Add clean fieldname validation to checkout form for full name field

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import Order
-
+from django.core.exceptions import ValidationError
 
 class OrderForm(forms.ModelForm):
     class Meta:
@@ -10,13 +10,12 @@ class OrderForm(forms.ModelForm):
                   'town_or_city', 'postcode', 'country',
                   'county',)
 
-    # def clean_full_name(self):
-    #     """
-    #     remove all whitespace and strip tags from full name
-    #     """
-    #     full_name = self.cleaned_data['full_name']
-    #     full_name = full_name.replace('&nbsp;', '').strip()
-    #     return full_name
+    def clean_full_name(self):
+        print('here!')
+        if not self.cleaned_data['full_name'].strip():
+            raise ValidationError("You have forgotten about Fred!")
+
+        return self.cleaned_data['full_name']
 
     def __init__(self, *args, **kwargs):
         """
@@ -45,6 +44,25 @@ class OrderForm(forms.ModelForm):
                 self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
-            
+
+        # self.fields['full_name'].widget.attrs.update(
+        #     {'pattern': '.*\\S+.*'})
+
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-gold'
+
+
+
+
+    # def clean(self):
+    #     """
+    #     remove all whitespace and strip tags from full name
+    #     """
+    #     full_name = self.cleaned_data['full_name'].strip()
+    #     return full_name
+
+    # def clean_fullname(self):
+    #     if not self.cleaned_data['full_name'].strip():
+    #         raise forms.ValidationError('Your error message here')
+
+            # full_name = full_name.replace('&nbsp;', '').strip()
