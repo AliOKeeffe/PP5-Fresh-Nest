f8074b95938142a120e68dec97b3eb16c0ef71d8
Alison O'Keeffe <okeeffe.ali@gmail.com>
2022-10-18 11:44:20 +0000

    Add Choices to enquiry type on contact model

--- contact/models.py
+++ contact/models.py
@@ -1,7 +1,6 @@
 """ Contact Models """
 
 from django.db import models
-from services.models import Service
 from django.urls import reverse
 
 # Create your models here.
@@ -9,11 +8,21 @@ from django.urls import reverse
 
 class Contact(models.Model):
     """ Model for Contact """
+
+    DESIGN_CONSULTATION = 'DS'
+    PRODUCT_QUERY = 'PQ'
+    ORDER_QUERY = 'OQ'
+    OTHER = 'O'
+    ENQUIRY_CHOICES = [
+        ('DESIGN_CONSULTATION', 'Design Consultation Enquiry'),
+        ('PRODUCT_QUERY', 'Product Query'),
+        ('ORDER_QUERY', 'Order Query'),
+        ('OTHER', 'Other')
+        ]
+
     name = models.CharField(max_length=122, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
-    enquiry_type = models.ForeignKey(
-        Service, on_delete=models.SET_NULL, null=True,
-        blank=True, related_name='contact')
+    enquiry_type = models.CharField(max_length=254, choices=ENQUIRY_CHOICES, default='DS')
     message = models.TextField(max_length=500, default='')
     date = models.DateTimeField(auto_now_add=True)
 
