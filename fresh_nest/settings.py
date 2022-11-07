5dffa1df70c887ee20f1548509460400bfbca665
Alison O'Keeffe <okeeffe.ali@gmail.com>
2022-11-07 16:37:12 +0000

    Set debug to false

--- fresh_nest/settings.py
+++ fresh_nest/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['fresh-nest.herokuapp.com', 'localhost']
